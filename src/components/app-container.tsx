eb0953773ded4fddfaf0d272b01f3998add893a2
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-12-19 17:20:00 +0600

    added layout

--- src/components/app-container.tsx
+++ src/components/app-container.tsx
@@ -6,7 +6,6 @@ const AppContainer = () => {
     <div className="max-w-screen-md mx-auto p-3">
       <div className="flex items-center justify-between">
         <div className="flex items-center">
-          {/* <img src={Logo} alt="logo"></img> */}
           <div className="text-xl text-primary">Todo</div>
         </div>
         <ThemeToggle></ThemeToggle>
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import AppContainer from './components/app-container';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import AppContainer from "./components/app-container";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-   <AppContainer />
-  </React.StrictMode>
-);
+const $root = document.getElementById("root") as HTMLElement;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+$root.className = "bg-light dark:bg-dark";
+
+ReactDOM.createRoot($root).render(<AppContainer />);
